ad4fcd144b1890e0aebe9d1c1b18a6db099124fb
murnana <anzu.ya0627@gmail.com>
2017-03-03 16:23:54 +0900

    add Project

---
+++ MacOSAppTraning01/MacOSAppTraning01/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacOSAppTraning01
+//
+//  Created by murnana on 2017/03/03.
+//  Copyright © 2017年 murnana. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacOSAppTraning01/MacOSAppTraning01/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOSAppTraning01
+//
+//  Created by murnana on 2017/03/03.
+//  Copyright © 2017年 murnana. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
